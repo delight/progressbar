c2ec7a0c8f2302bc0282ed408e469214ce52a5fc
Konstantinos Kostarellis <dev@kostarellis.com>
2022-12-06 23:02:35 +0100

    demo for the progressbar dependency problem

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a class="btn btn-sm btn-ghost-surface" href="/">Home</a>
+				<a class="btn btn-sm btn-ghost-surface" href="demo3">Demo 3</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/demo3/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Modal, modalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+
+	function triggerAlert(): void {
+		const alert: ModalSettings = {
+			type: 'alert',
+			title: 'Example Alert',
+			body: 'This is an example modal.',
+			image: 'https://i.imgur.com/WOgTG96.gif'
+		};
+		modalStore.trigger(alert);
+	}
+</script>
+
+<Modal />
+
+<pre>queue: {JSON.stringify($modalStore, null, 2)}</pre>
+<button on:click={triggerAlert} class="btn btn-ghost-accent">Open Modal</button>
